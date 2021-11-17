dd80041de9f380e0ff8db03e91f1636a157f4530
VINOD ANIL PAWAR <84064563+vinod12796@users.noreply.github.com>
2021-11-17 13:35:36 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+
+const CheckPer = () => {
+
+     var loveData = Math.random() * 100;
+     loveData = Math.floor(loveData);
+     document.getElementById("loveValue").value = loveData + "%";
+     document.getElementById("loveValue1").value = loveData + "%";
+
+}
+
+
