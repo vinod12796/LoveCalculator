fefca7037f636dbd14cc040562c0e0174314c491
VINOD ANIL PAWAR <84064563+vinod12796@users.noreply.github.com>
2021-11-17 14:02:10 +0530

    Validation updated

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 
 const CheckPer = () => {
 
-     var loveData = Math.random() * 100;
-     loveData = Math.floor(loveData);
-     document.getElementById("loveValue").value = loveData + "%";
-     document.getElementById("loveValue1").value = loveData + "%";
+     var name = document.getElementById('name').value;
+     var lname = document.getElementById('lname').value;
+
+     if (name == "") {
+          alert("Please Enter Your name");
+     } else if (name.length <= 2) {
+          alert("Min Lenghth is 3 ");
+     } else if (!isNaN(name)) {
+          alert('Enter Correct Name ')
+     }
+
+     else if (lname == "") {
+          alert("Please Enter Your name");
+     } else if (lname.length <= 2) {
+          alert("Min Lenghth is 3 ");
+     } else if (!isNaN(lname)) {
+          alert('Enter Correct Name ')
+     }
+     else {
+          var loveData = Math.random() * 100;
+          loveData = Math.floor(loveData);
+          document.getElementById("loveValue").value = loveData + "%";
+          document.getElementById("loveValue1").value = loveData + "%";
+     }
+
 
 }
+
+
 
 
